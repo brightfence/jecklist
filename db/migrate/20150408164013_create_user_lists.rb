289b95c42f227e2598b98c122ed18b952f1a85e3
Jonathan Cho <joncho26@gmail.com>
2015-04-08 12:54:18 -0400

    initial commit with rails skeleton and associations

---
+++ db/migrate/20150408164013_create_user_lists.rb
@@ -0,0 +1,12 @@
+class CreateUserLists < ActiveRecord::Migration
+  def change
+    create_table :user_lists do |t|
+      t.references :user, index: true
+      t.references :list, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :user_lists, :users
+    add_foreign_key :user_lists, :lists
+  end
+end
