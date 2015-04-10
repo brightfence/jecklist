5231d0df108f363ff7391d55cbdacc4a55cfca4b
Jonathan Cho <joncho26@gmail.com>
2015-04-10 12:38:11 -0400

    add additional seeds to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,20 @@ jon = User.create(name:"jon",password:'password')
 
 pearl = User.create(name:'pearl',password:'password')
 
+sam = User.create(name:'sam', password:'password')
+
 list1 = List.create(title:"First ToDo List")
+list2 = List.create(title:"Finish JeckList MVP")
 
 UserList.create(user:jon,list:list1)
 UserList.create(user:pearl, list:list1)
 
 UserTask.create(user_id:1,task_id:1)
+
+UserList.create(user:jon,list: list2)
+UserList.create(user:sam,list: list2)
+
+
 
 
 
