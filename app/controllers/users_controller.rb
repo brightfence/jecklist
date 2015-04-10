0b0230c0e840abc72173e8ddf7161b0547e75a8f
Pearl Shin <pearlshin22@gmail.com>
2015-04-10 13:19:19 -0400

    add create action for new user in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to :back
+      flash[:notice] = "There was an error with your signup."
+    end
+  end
+
   def edit
   end
 
