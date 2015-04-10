e1067b529a2f86c91e6b629069a20745fde8fb7c
Pearl Shin <pearlshin22@gmail.com>
2015-04-10 17:15:44 -0400

    add sessions helper to see if logout issue gets resolved

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+
+
+end
